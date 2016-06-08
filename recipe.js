a03fb915e5306a4f2664e4ac5925e4e55c8b9f0f
John Carlo Malto <jakemalto2014@gmail.com>
2016-06-08 14:47:27 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient (playlist, name, title){
+	playlist[name] = title;
+	return playlist;
+}
+
+function removeIngredient(object, name){
+	delete object[name];
+	return object;
+}
+
+function updateIngredient(object, ing, amount){
+  object[ing] = amount
+  return object
+}
+
+function readRecipe(object){
+  	for (var key in object) {
+  		console.log("this recipe calls for "+ object[key] +" of "+ key)
+  	}
+}
